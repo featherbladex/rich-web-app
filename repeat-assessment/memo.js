887368ac4addd5caab4c5d84dcc149b57bca92d2
featherbladex <57197043+featherbladex@users.noreply.github.com>
2023-07-06 18:17:56 +0100

    attempts at react

--- repeat-assessment/memo.js
+++ repeat-assessment/memo.js
@@ -1,2 +1,11 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+function Hello() {
+    return <h1>Hello World!</h1>;
+  }
+
+  const container = document.getElementById('mydiv');
+  const root = ReactDOM.createRoot(container);
+  root.render(<Hello />)
