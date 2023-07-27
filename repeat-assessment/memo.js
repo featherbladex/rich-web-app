4e0e0357f39ba4d396472b8e8ff243f088edbbf8
featherbladex <57197043+featherbladex@users.noreply.github.com>
2023-07-27 16:10:35 +0100

    rework

--- repeat-assessment/memo.js
+++ repeat-assessment/memo.js
@@ -1,11 +1,79 @@
+//buttons
+let add_button = document.getElementById('add');
 
-import React from "react";
-import ReactDOM from "react-dom/client";
+add_button.onclick=add();
 
-function Hello() {
-    return <h1>Hello World!</h1>;
+
+
+function get_notes(){
+  let note_array = new Array;
+  let note_str = localStorage.getItem('content');
+
+  if(note_str !== null){
+    note_array = JSON.parse(note_str);
+  }
+  return note_array;
+}
+
+function add(){
+  let note = document.getElementById('note').value;
+  let notes = get_notes();
+  let new_item = {
+    name:note,
+    id: "item" + notes.length
+  };
+  notes.push(new_item);
+  
+  localStorage.setItem('content',JSON.stringify(notes));
+  show();
+  return false;
+}
+  
+
+function del(){
+  let id = value;
+  let notes = get_notes();
+
+  notes.splice(id,1);
+  localStorage.setItem('content',JSON.stringify(notes));
+  show();
+  return false;
+}
+
+function edit(value){
+  let id = value;
+  let notes = get_notes();
+  let rewrite = prompt("Note:","Edit");
+  for(let i=0;i<notes.length;i++){
+    let content= notes[i];
+    console.log(content);
+    if(content.id==id){
+      content.name=rewrite;
+    }
   }
 
-  const container = document.getElementById('mydiv');
-  const root = ReactDOM.createRoot(container);
-  root.render(<Hello />)
+  show();
+  return false;
+}
+
+function show(){
+  let notes = get_notes();
+  let html = '<p>';
+
+  for(let i =0;i<notes.length;i++){
+    let content = notes[i];
+    html += "<p id ='item"+i+"'>"+content["name"]+ 
+    '<p>'+"<button class = 'del'> Delete </button>"+
+    " "+"<button class = 'edit' value = 'item"+i+ "'> Edit </button>";
+  };
+
+  document.getElementById('item').innerHTML = html;
+
+  //let delete_button = document.getElementsByClassName('del');
+  
+  //let edit_button = document.getElementsByClassName('edit');
+  
+
+}
+
+
